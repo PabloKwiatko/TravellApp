97bbb58cc34bda7a1454ec546c209fc590d80cc7
PabloKwiatko <pawelk@live.com>
2025-10-26 13:31:39 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -55,6 +55,11 @@ def translate_text(text, target_lang):
             "pl": "park",
             "en": "park",
             "no": "park"
+        },
+        "stadium": {
+            "pl": "stadion",
+            "en": "stadium",
+            "no": "stadion"
         }
     }
     return translations.get(text.lower(), {}).get(target_lang, text)
@@ -71,4 +76,49 @@ def search_google_places(country, category, city=None, location=None, radius=600
         f"https://maps.googleapis.com/maps/api/place/textsearch/json?"
         f"query={query}{loc_str}&key={API_KEY}&language={lang}"
     )
-    results
+    results = requests.get(url).json().get("results", [])
+    return [
+        {
+            "name": r["name"],
+            "translated_category": translate_text(category, lang),
+            "address": r.get("formatted_address", ""),
+            "photo": r.get("photos", [{}])[0].get("photo_reference", ""),
+            "location": r["geometry"]["location"],
+            "rating": r.get("rating", None),
+        }
+        for r in results
+    ]
+
+@app.get("/travel-plan")
+def get_travel_plan(
+    country: str,
+    city: str = Query(default=None),
+    category: str = Query(default="temple"),
+    transport: str = Query(default="Motocykl"),
+    radius: int = Query(default=60000),
+    lang: str = Query(default="pl", enum=["pl", "en", "no"])
+):
+    places = search_google_places(country, category, city=city, radius=radius, lang=lang)
+    if not places:
+        return {
+            "message": NO_RESULTS_MESSAGES.get(lang, NO_RESULTS_MESSAGES["pl"]),
+            "country": country,
+            "city": city,
+            "category": category,
+            "category_translated": translate_text(category, lang),
+            "transport": transport,
+            "radius": radius,
+            "language": lang,
+            "places": []
+        }
+    else:
+        return {
+            "country": country,
+            "city": city,
+            "category": category,
+            "category_translated": translate_text(category, lang),
+            "transport": transport,
+            "radius": radius,
+            "language": lang,
+            "places": places
+        }
